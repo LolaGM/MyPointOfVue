981cb761f4063ccc4b04a73182592ab35c84b7ac
LolaGM <canal030w@gmail.com>
2023-02-24 14:38:03 +0100

    first commit

---
+++ src/components/myComponents/SingleComponent.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <div>
+        <h3>My Single Component for FEMCODERS P1 Málaga</h3>
+    </div>
+  
+</template>
+
+<style language="CSS" scoped>
+h3{
+    color:#42b883;
+}
+
+</style>
---
+++ src/views/MyView.vue
@@ -0,0 +1,33 @@
+<script>
+import SingleComponent from '../components/myComponents/SingleComponent.vue'
+export default {
+  components: { SingleComponent },
+
+}
+</script>
+
+<template>
+    <div>
+        <h1>My Single Page with VUE3 by FEMCODERS</h1>
+        <p>Las FEMCODERS P1 Málaga estamos aprendiendo desde cero a usar VUE3. </p>
+        <p>Nuestra primera vista en VUE está hecha siguiendo las rutas indicadas en el archivo index.js de la carpeta router.</p>
+        <p>No olvidemos ir al archivo App.vue para incluir un enlace a la vista en el menú usando ROUTERLINK y TO para indicar el PATH y el nombre de la vista.</p>
+        <p>Usando el atributo SCOPED en la etiqueta STYLE conseguiremos que el estilo sólo afecte al archivo .vue que estamos creando.</p>
+        -----------------------------------------------------------
+        <SingleComponent/>
+        -----------------------------------------------------------
+        <SingleComponent/>
+
+    </div>
+</template>
+
+<style language="CSS" scoped>
+h1{
+    color:#42b883;
+}
+
+p{
+    color: #34495e;
+}
+
+</style>
